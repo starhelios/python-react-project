fd9962889587713e731f096e9a712291583648dd
starhelios <starhelios94@gmail.com>
2019-04-22 15:10:26 -0400

    updated mathpix-markdown

--- mathpix-markdown/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import  MathpixLoader from './components/mathpix-loader';
-import  MathpixMarkdown from './components/mathpix-markdown';
-import {MathpixMarkdownModel} from './mathpix-markdown-model'
-
-
-export {MathpixLoader, MathpixMarkdown, MathpixMarkdownModel};
-
