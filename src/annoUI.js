a2dd5a52c8a4150fa63cd0086814e9c5de3a8b44
Admin <>
2019-04-30 13:00:06 -0400

    CharSize -> ChatSizeTmp

--- src/annoUI.js
+++ src/annoUI.js
@@ -172,12 +172,12 @@ class AnnotationUI extends Component {
     let eventTypeFinal = eventType
 
     if (eventType === 'CharSizePlus') {
-      annotation.charSize = annotation.charSize * 1.1;
+      annotation.charSize = annotation.charSize * 1.17;
       eventTypeFinal = 'Updated';
     }
 
     if (eventType === 'CharSizeMinus') {
-      annotation.charSize = annotation.charSize * 0.9;
+      annotation.charSize = annotation.charSize * 0.83;
       eventTypeFinal = 'Updated';
     }
 
