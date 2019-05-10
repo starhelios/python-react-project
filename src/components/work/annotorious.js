c6cacf73dc74a3e1ee77a1cd390982da553a8983
Pavel Chesovsky <pavel.chesnovsky@gmail.com>
2019-05-11 02:46:06 +0300

    #22 - Show text values on hover if they exist

--- src/components/work/annotorious.js
+++ src/components/work/annotorious.js
@@ -92,7 +92,7 @@ export default class Annotorious extends Component {
     if (this.props.imageURL) {
       return (
         <div className="text-center anno-board">
-          <div className="wrapper-annotatable" style={{height: wrapperHeight}}>
+          <div className="wrapper-annotatable" style={{height: wrapperHeight, paddingTop: 50}}>
             <img id="mainImage" ref="mainImage" src={this.props.imageURL} className="annotatable"
               width={this.props.imageWidth} height={this.props.imageHeight}
               onLoad={this.update} />
